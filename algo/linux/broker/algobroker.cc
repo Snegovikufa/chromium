e51a7473c9e7a8f142139aa432fa46c065b346ed
Alex <a.rttr@yandex.ru>
2021-09-26 21:11:19 +0300

    remove commented out code in algobroker

---
+++ algo/linux/broker/algobroker.cc
@@ -0,0 +1,38 @@
+#include "algo/linux/broker/algobroker.h"
+
+#include <iostream>
+#include <signal.h>
+
+#include "base/at_exit.h"
+#include "sandbox/linux/services/namespace_sandbox.h"
+#include "sandbox/policy/sandbox.h"
+#include "sandbox/policy/sandbox_type.h"
+#include "sandbox/policy/linux/sandbox_linux.h"
+
+
+using sandbox::syscall_broker::BrokerFilePermission;
+using sandbox::syscall_broker::MakeBrokerCommandSet;
+
+bool callback(sandbox::policy::SandboxLinux::Options options) {
+    return true;
+}
+
+int main(int argc, char** argv) {
+  std::cout << "AlgoBroker started" << std::endl;
+
+  new base::AtExitManager();
+
+  if (base::CommandLine::Init(argc, argv)) {
+    const std::string path = argv[1];
+    const auto file_path = base::FilePath(path);
+    const auto command_line = base::CommandLine(file_path);
+
+    base::LaunchOptions launch_options;
+    launch_options.wait = true;
+
+    sandbox::NamespaceSandbox::LaunchProcess(command_line, launch_options);
+  }
+
+  std::cout << "AlgoBroker finished" << std::endl;
+  return 0;
+}
