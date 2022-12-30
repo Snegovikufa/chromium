68f80f12417b10e2da7a0f2e68be2ceddae6d8d3
Alex <a.rttr@yandex.ru>
2022-12-29 19:54:35 -0600

    remove event and reg rules parsing

--- algo/win/broker/host.cc
+++ algo/win/broker/host.cc
@@ -23,8 +23,6 @@
 #define PACKAGE "packageName"
 #define FS_RULES "filesystemRules"
 #define PIPE_RULES "pipeRules"
-#define EVENT_RULES "eventRules"
-#define REG_RULES "registryRules"
 #define PATTERN "pattern"
 #define RO "readOnly"
 #define WIDE_SPACE std::wstring(L" ")
@@ -122,8 +120,6 @@ int run_broker_main(int argc, wchar_t** argv) {
         const auto package_name = get_value(PACKAGE, root);
         const auto fs_rules = get_value(FS_RULES, root);
         const auto pipe_rules = get_value(PIPE_RULES, root);
-        const auto event_rules = get_value(EVENT_RULES, root);
-        const auto reg_rules = get_value(REG_RULES, root);
         const auto cmd = std::wstring(L"target ") + target + WIDE_SPACE + args;
 
         algo::TargetInformation* target_result = new algo::TargetInformation;
