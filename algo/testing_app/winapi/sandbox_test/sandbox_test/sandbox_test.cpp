9e2dfa8083b5b218463d91ca932d76f7c5bfeb50
Alex <a.rttr@yandex.ru>
2023-01-04 21:10:18 -0600

    add error handling and last error information to sandbox test

--- algo/testing_app/winapi/sandbox_test/sandbox_test/sandbox_test.cpp
+++ algo/testing_app/winapi/sandbox_test/sandbox_test/sandbox_test.cpp
@@ -1,26 +1,50 @@
 #include <windows.h>
+
 #include <iostream>
+#include <string>
+
+std::string GetLastErrorAsString() {
+  DWORD errorMessageID = ::GetLastError();
+  if (errorMessageID == 0) {
+    return std::string();
+  }
+
+  LPSTR messageBuffer = nullptr;
+  size_t size = FormatMessageA(
+      FORMAT_MESSAGE_ALLOCATE_BUFFER
+        | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+      NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+      (LPSTR)&messageBuffer, 0, NULL);
+
+  std::string message(messageBuffer, size);
+  LocalFree(messageBuffer);
+
+  return message;
+}
 
 int main()
 {
   HKEY hKey;
   DWORD dwDisposition;
-  LPCWSTR key = L"sandbox_test";
+  std::wstring key = L"sandbox_test";
+  LPCWSTR lpsKey = key.c_str();
   if (RegCreateKeyEx(HKEY_LOCAL_MACHINE,
-                     key, 0,
+                     lpsKey, 0,
                      NULL, 0, KEY_WRITE, NULL, &hKey,
                      &dwDisposition) == ERROR_SUCCESS) {
-    std::cout << "Key " << key << " has been written successfully" << std::endl;
+    std::wcout << L"Key " << key << L" has been written successfully" << std::endl;
     RegCloseKey(hKey);
   }
   else {
-    std::cout << "Create key " << key << " has failed" << std::endl;
+    std::wcout << L"Create key " << key << L" has failed" << std::endl;
+    std::cerr << GetLastErrorAsString() << std::endl;
     return -1;
   }
 
-  LPCWSTR file = L"C:\\sandbox_test.txt";
+  std::wstring file = L"C:\\sandbox_test.txt";
+  LPCWSTR lpsFile = L"C:\\sandbox_test.txt";
   HANDLE hFile = CreateFile(
-      file,
+      lpsFile,
       GENERIC_WRITE,
       FILE_SHARE_WRITE,
       NULL,
@@ -28,10 +52,12 @@ int main()
       FILE_ATTRIBUTE_NORMAL,
       NULL);
   if (hFile == INVALID_HANDLE_VALUE) {
+    std::wcout << L"Failed to create file " << file << std::endl;
+    std::cerr << GetLastErrorAsString() << std::endl;
     return -2;
   }
   else {
-    std::cout << "File " << file << " has been created successfully" << std::endl;
+    std::wcout << L"File " << file << L" has been created successfully" << std::endl;
     CloseHandle(hFile);
   }
 
