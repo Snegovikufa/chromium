e5d1fba74322fddae33748beed69e0da2b7650bb
Alex <a.rttr@yandex.ru>
2023-01-21 21:33:18 -0600

    Revert "remove event and reg rules parsing"
    This reverts commit 68f80f12417b10e2da7a0f2e68be2ceddae6d8d3.

--- algo/win/broker/host.cc
+++ algo/win/broker/host.cc
@@ -23,6 +23,8 @@
 #define PACKAGE "packageName"
 #define FS_RULES "filesystemRules"
 #define PIPE_RULES "pipeRules"
+#define EVENT_RULES "eventRules"
+#define REG_RULES "registryRules"
 #define PATTERN "pattern"
 #define RO "readOnly"
 #define WIDE_SPACE std::wstring(L" ")
@@ -120,6 +122,8 @@ int run_broker_main(int argc, wchar_t** argv) {
         const auto package_name = get_value(PACKAGE, root);
         const auto fs_rules = get_value(FS_RULES, root);
         const auto pipe_rules = get_value(PIPE_RULES, root);
+        const auto event_rules = get_value(EVENT_RULES, root);
+        const auto reg_rules = get_value(REG_RULES, root);
         const auto cmd = std::wstring(L"target ") + target + WIDE_SPACE + args;
 
         algo::TargetInformation* target_result = new algo::TargetInformation;
