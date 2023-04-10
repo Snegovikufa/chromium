b71c09d5bb3bb34dffea83cb250d62db2a5b17e6
Alex <a.rttr@yandex.ru>
2023-04-09 22:11:53 -0600

    add env file

---
+++ algo/darwin/host/env.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+export __CT_PRODUCT_PATH
+export __CT_DOTNET_PATH
+export __CT_HOSTFXR_PATH=/usr/local/share/dotnet/host/fxr/6.0.15/libhostfxr.dylib
+
+export __CT_ALGOHOST_ENDPOINT_DIR
+export __CT_ALGOHOST_ENDPOINT_ASM
+export __CT_ALGOHOST_ENDPOINT_CONFIG
+export __CT_ALGOHOST_ENDPOINT_TYPE
+export __CT_ALGOHOST_ENDPOINT_METHOD
+export __CT_ALGOHOST_ENDPOINT_PRELOAD_METHOD
